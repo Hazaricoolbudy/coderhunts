06f341f52869c6ff70907be07afac2e4ea3b0761
hazari <hazarisah48@gmail.com>
2022-04-25 17:55:45 +0530

    cleanup

---
+++ pages/blogs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div>
+    this is blog pages
+      
+    </div>
+  )
+}
+
+export default Blogs
---
+++ pages/contect.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contect = () => {
+  return (
+    <div>
+      This is Contect pages
+    </div>
+  )
+}
+
+export default Contect
